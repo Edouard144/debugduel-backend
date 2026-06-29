3660a4d5351760d8f1b110e773c80f0e343d6f82
Edouard <edouardtuyubahe@gmail.com>
2026-06-29 11:32:30 +0200

    Fixing the render issues of DJANGO_SETTINGS_MODULE and get_asgi_application() that must come before any Django model imports

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,16 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import duels.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             duels.routing.websocket_urlpatterns
