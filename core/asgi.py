b6bb149303dc61d213f9d16172cfa38ab5231774
Edouard <edouardtuyubahe@gmail.com>
2026-06-29 11:42:04 +0200

    Fixing the render issues

--- core/asgi.py
+++ core/asgi.py
@@ -3,11 +3,12 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import duels.routing
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
